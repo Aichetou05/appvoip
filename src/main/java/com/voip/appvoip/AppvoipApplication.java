9a091f57332548226995940e3030406b58b33f6c
Aichetou05 <23021@supnum.mr>
2025-07-03 14:37:02 +0200

    first commit:creation environnement meavn pour app voip

---
+++ src/main/java/com/voip/appvoip/AppvoipApplication.java
@@ -0,0 +1,13 @@
+package com.voip.appvoip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppvoipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppvoipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voip/appvoip/AppvoipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voip.appvoip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppvoipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
